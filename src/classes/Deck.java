9144bc446849b1e5f263546633d0dce085aaacbc
Damir <serazetdinow.damir@yandex.ru>
2016-12-09 23:01:55 +0300

    Method shuffle in Deck added.

--- src/classes/Deck.java
+++ src/classes/Deck.java
@@ -4,6 +4,7 @@ import entities.Card;
 import entities.Color;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -12,52 +13,54 @@ import java.util.List;
 public class Deck {
     private List<Card> cards;
 
-    public Deck() {
+    public Deck(boolean clear) {
         cards = new ArrayList<>();
-        int id = 1;
-        cards.add(new Card(id, 0, Color.RED));
-        id++;
-        cards.add(new Card(id, 0, Color.YELLOW));
-        id++;
-        cards.add(new Card(id, 0, Color.GREEN));
-        id++;
-        cards.add(new Card(id, 0, Color.BLUE));
-        id++;
-        for (int j = 0; j < 2; j++) {
-            for (int i = 1; i < 12; i++) {
-                cards.add(new Card(id, i, Color.RED));
-                id++;
-            }
-        }
-        for (int j = 0; j < 2; j++) {
-            for (int i = 1; i < 12; i++) {
-                cards.add(new Card(id, i, Color.YELLOW));
-                id++;
-            }
-        }
-        for (int j = 0; j < 2; j++) {
-            for (int i = 1; i < 12; i++) {
-                cards.add(new Card(id, i, Color.GREEN));
-                id++;
-            }
-        }
-        for (int j = 0; j < 2; j++) {
-            for (int i = 1; i < 12; i++) {
-                cards.add(new Card(id, i, Color.BLUE));
-                id++;
-            }
-        }
-        for (int i = 0; i < 4; i++){
-            cards.add(new Card(id, 13, Color.BLACK));
+        if (clear) {
+            int id = 1;
+            cards.add(new Card(id, 0, Color.RED));
             id++;
-        }
-        for (int i = 0; i < 4; i++){
-            cards.add(new Card(id, 14, Color.BLACK));
+            cards.add(new Card(id, 0, Color.YELLOW));
             id++;
+            cards.add(new Card(id, 0, Color.GREEN));
+            id++;
+            cards.add(new Card(id, 0, Color.BLUE));
+            id++;
+            for (int j = 0; j < 2; j++) {
+                for (int i = 1; i < 12; i++) {
+                    cards.add(new Card(id, i, Color.RED));
+                    id++;
+                }
+            }
+            for (int j = 0; j < 2; j++) {
+                for (int i = 1; i < 12; i++) {
+                    cards.add(new Card(id, i, Color.YELLOW));
+                    id++;
+                }
+            }
+            for (int j = 0; j < 2; j++) {
+                for (int i = 1; i < 12; i++) {
+                    cards.add(new Card(id, i, Color.GREEN));
+                    id++;
+                }
+            }
+            for (int j = 0; j < 2; j++) {
+                for (int i = 1; i < 12; i++) {
+                    cards.add(new Card(id, i, Color.BLUE));
+                    id++;
+                }
+            }
+            for (int i = 0; i < 4; i++) {
+                cards.add(new Card(id, 13, Color.BLACK));
+                id++;
+            }
+            for (int i = 0; i < 4; i++) {
+                cards.add(new Card(id, 14, Color.BLACK));
+                id++;
+            }
         }
     }
 
     private void reshuffle(){
-        
+        Collections.shuffle(cards);
     }
 }
